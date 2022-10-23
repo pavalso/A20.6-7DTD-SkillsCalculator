0a768afb772db1ed881fe50f8bb0693ccd4bded8
pavalso <92272697+pavalso@users.noreply.github.com>
2022-10-23 21:46:55 +0200

    Updated

--- js/testjson.js
+++
@@ -1,8 +0,0 @@
-let nav_options = document.getElementsByClassName("selectable-icon");
-
-for(let i = 0; i < nav_options.length; i++){
-  let element = nav_options[i];
-  element.addEventListener("click", () => {
-    console.log(element.id);
-  });
-}
